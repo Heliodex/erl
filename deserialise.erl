c1dae2ac5facae77124ed18f7803e92f37a87c9d
Lewin Kelly <heli@odex.cf>
2025-08-04 07:43:08 +0100

    Basic proto and instruction reading code

--- deserialise.erl
+++ deserialise.erl
@@ -1,13 +1,17 @@
 -module(deserialise).
 -export([start/0]).
 
-rByte(Binary) ->
-    <<B:8, Rest/binary>> = Binary,
+rByte(<<B:8, Rest/binary>>) ->
     {B, Rest}.
 
-rBool(Binary) ->
-    {B, Rest} = rByte(Binary),
+rBool(<<B:8, Rest/binary>>) ->
     {B /= 0, Rest}.
+
+rUint32(<<W:32, Rest/binary>>) ->
+    {W, Rest}.
+
+skipUint32(<<_:32, Rest/binary>>) ->
+    Rest.
 
 rVarInt(Binary, I, R) ->
     <<V:8, Rest/binary>> = Binary,
@@ -24,9 +28,7 @@ rVarInt(Binary) ->
     {Rest, _, R} = rVarInt(Binary, 0, 0),
     {R, Rest}.
 
-skipVarInt(Binary, I) ->
-    <<V:8, Rest/binary>> = Binary,
-
+skipVarInt(<<V:8, Rest/binary>>, I) ->
     if
         V band 2#1000_0000 == 0; I == 3 ->
             {Rest, I + 1};
@@ -63,6 +65,47 @@ userdataTypeRemapping(Binary) ->
             Rest
     end.
 
+readInst(Binary) ->
+    {Value, Rest} = rUint32(Binary),
+    {[], false, Rest}.
+
+readInsts(Binary, 0, Code) ->
+    {Code, Binary};
+readInsts(Binary, Sizecode, Code) ->
+    case readInst(Binary) of
+        {Insts, true, Rest} ->
+            readInsts(Rest, Sizecode - 2, Insts ++ Code);
+        {Insts, false, Rest} ->
+            readInsts(Rest, Sizecode - 1, Insts ++ Code)
+    end.
+
+readInsts(Binary, Sizecode) ->
+    readInsts(Binary, Sizecode, []).
+
+readProto(Binary, StringList) ->
+    {MaxStackSize, Rest1} = rByte(Binary),
+    {NumParams, Rest2} = rByte(Rest1),
+    {Nups, Rest3} = rByte(Rest2),
+
+    <<_:16, Rest4/binary>> = Rest3,
+    {Typesize, Rest5} = rVarInt(Rest4),
+    <<_:Typesize/binary, Rest6/binary>> = Rest5,
+    {Sizecode, Rest7} = rVarInt(Rest6),
+
+    {Code, Rest8} = readInsts(Rest7, Sizecode),
+
+    {1, Rest8}.
+
+readProtos(Binary, _, 0, Protos) ->
+    {Protos, Binary};
+readProtos(Binary, StringList, Count, Protos) ->
+    {Proto, Rest} = readProto(Binary, StringList),
+    io:format("Proto: ~p~n", [Proto]),
+    readProtos(Rest, StringList, Count - 1, Protos ++ [Proto]).
+
+readProtos(Binary, StringList, Count) ->
+    readProtos(Binary, StringList, Count, []).
+
 read_file(Filename) ->
     case file:read_file(Filename) of
         {ok, Binary} ->
@@ -96,9 +139,11 @@ main() ->
     io:format("Strings: ~p~n", [StringList]),
 
     Rest4 = userdataTypeRemapping(Rest3),
-	{ProtoCount, Rest5} = rVarInt(Rest4),
+    {ProtoCount, Rest5} = rVarInt(Rest4),
+    io:format("Proto count: ~p~n", [ProtoCount]),
 
-    io:format("Rest: ~p~n", [Rest5]),
+    {ProtoList, Rest6} = readProtos(Rest5, StringList, ProtoCount),
+    io:format("Rest: ~p~n", [Rest6]),
 
     io:format("Deserialisation completed~n").
 
