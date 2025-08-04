3719fcaf5af490971b948de941b5459845404536
Lewin Kelly <heli@odex.cf>
2025-08-04 06:41:01 +0100

    Read bytes, booleans, and userdata type remappings

--- deserialise.erl
+++ deserialise.erl
@@ -1,14 +1,20 @@
 -module(deserialise).
 -export([start/0]).
 
+rByte(Binary) ->
+    <<B:8, Rest/binary>> = Binary,
+    {B, Rest}.
+
+rBool(Binary) ->
+    {B, Rest} = rByte(Binary),
+    {B /= 0, Rest}.
+
 rVarInt(Binary, I, R) ->
     <<V:8, Rest/binary>> = Binary,
 
     NewR = R bor (V band 2#0111_1111) bsl (I * 7),
     if
-        V band 2#1000_0000 == 0 ->
-            {Rest, I + 1, NewR};
-        I == 3 ->
+        V band 2#1000_0000 == 0; I == 3 ->
             {Rest, I + 1, NewR};
         true ->
             rVarInt(Rest, I + 1, NewR)
@@ -18,25 +24,44 @@ rVarInt(Binary) ->
     {Rest, _, R} = rVarInt(Binary, 0, 0),
     {R, Rest}.
 
+skipVarInt(Binary, I) ->
+    <<V:8, Rest/binary>> = Binary,
+
+    if
+        V band 2#1000_0000 == 0; I == 3 ->
+            {Rest, I + 1};
+        true ->
+            skipVarInt(Rest, I + 1)
+    end.
+
+skipVarInt(Binary) ->
+    {Rest, _} = skipVarInt(Binary, 0),
+    Rest.
+
 rString(Binary) ->
     {Size, Rest} = rVarInt(Binary),
-	io:format("String size: ~p~n", [Size]),
+    io:format("String size: ~p~n", [Size]),
     <<Str:Size/binary, Rest2/binary>> = Rest,
     {binary_to_list(Str), Rest2}.
 
 % Erlang handles bitstrings really nicely, still getting the hang of looping with recursion tho
+rStrings(Binary, 0, Strings) ->
+    {Strings, Binary};
 rStrings(Binary, Count, Strings) ->
-	if
-		Count == 0 ->
-			{lists:reverse(Strings), Binary};
-		true ->
-			{Str, Rest} = rString(Binary),
-			io:format("String: ~p~n", [Str]),
-			rStrings(Rest, Count - 1, [Str | Strings])
-	end.
+    {Str, Rest} = rString(Binary),
+    io:format("String: ~p~n", [Str]),
+    rStrings(Rest, Count - 1, Strings ++ [Str]).
 
 rStrings(Binary, Count) ->
-	rStrings(Binary, Count, []).
+    rStrings(Binary, Count, []).
+
+userdataTypeRemapping(Binary) ->
+    case rBool(Binary) of
+        {true, Rest} ->
+            userdataTypeRemapping(skipVarInt(Rest));
+        {false, Rest} ->
+            Rest
+    end.
 
 read_file(Filename) ->
     case file:read_file(Filename) of
@@ -70,7 +95,10 @@ main() ->
     {StringList, Rest3} = rStrings(Rest2, StringCount),
     io:format("Strings: ~p~n", [StringList]),
 
-    io:format("Rest: ~p~n", [Rest3]),
+    Rest4 = userdataTypeRemapping(Rest3),
+	{ProtoCount, Rest5} = rVarInt(Rest4),
+
+    io:format("Rest: ~p~n", [Rest5]),
 
     io:format("Deserialisation completed~n").
 
